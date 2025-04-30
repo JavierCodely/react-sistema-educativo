6b32b83998831c651f9361e9d25a2d9089f633f5
JavierCodely <javiercodely@gmail.com>
2025-04-30 09:13:03 -0300

    negro

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import StudentDashboard from "./pages/alumno/StudentDashboard";
 import ProtectedRoute from "./routes/protectedRoute";
 import NotificacionesPage from "./pages/notificaciones/NotificacionesPage";
 import { NotificacionesProvider } from "./contexts/NotificacionesContext";
+
+useEffect(() => {
+  document.documentElement.setAttribute("data-bs-theme", "light");
+  document.body.classList.remove("bg-dark", "text-white");
+  document.body.classList.add("bg-light", "text-dark");
+}, []);
+
 function App() {
   return (
     <NotificacionesProvider>
